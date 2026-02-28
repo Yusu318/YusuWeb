228c15d7823e903175c742e835f61ea48e15dee5
Yusu318 <yusu.wang01@whitireian.ac.nz>
2026-03-01 12:00:29 +1300

    finish exercise

---
+++ YusuWeb/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using YusuWeb.Models;
+
+namespace YusuWeb.Data
+{
+    public class ApplicationDbContext:DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options): base(options)
+            {
+
+            }
+        public DbSet<Category> Categories { get; set; }
+    }
+}
