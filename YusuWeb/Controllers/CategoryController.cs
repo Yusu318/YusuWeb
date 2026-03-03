6390e36115f467f21b5cde80ebaf7fa464d8ec05
Yusu318 <yusu.wang01@whitireian.ac.nz>
2026-03-03 20:54:20 +1300

    到最后一页了，但是VS崩了

--- YusuWeb/Controllers/CategoryController.cs
+++ YusuWeb/Controllers/CategoryController.cs
@@ -17,12 +17,18 @@ namespace YusuWeb.Controllers
         {
             //var objCategoryList= _db.Categories;
             List<Category> objCategoryList = _db.Categories.ToList();
-
             return View(objCategoryList);
         }
         public IActionResult Create()
         {
             return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Category obj)
+        {
+            _db.Categories.Add(obj);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
         }
     }
 }
